aed8edd33f00f209540f203814f34d28c434ccef
Marc Reichelt <mcreichelt@gmail.com>
2017-08-16 23:28:47 +0200

    solve global kinematic model

--- global_kinematic_model/src/main.cpp
+++ global_kinematic_model/src/main.cpp
@@ -7,33 +7,47 @@
 // Helper functions
 //
 constexpr double pi() { return M_PI; }
+
 double deg2rad(double x) { return x * pi() / 180; }
+
 double rad2deg(double x) { return x * 180 / pi(); }
 
 const double Lf = 2;
 
-// TODO: Implement the global kinematic model.
 // Return the next state.
 //
 // NOTE: state is [x, y, psi, v]
 // NOTE: actuators is [delta, a]
 Eigen::VectorXd globalKinematic(Eigen::VectorXd state,
                                 Eigen::VectorXd actuators, double dt) {
-  Eigen::VectorXd next_state(state.size());
-  return next_state;
+    Eigen::VectorXd next_state(state.size());
+
+    double x = state[0],
+            y = state[1],
+            psi = state[2],
+            v = state[3],
+            delta = actuators[0],
+            a = actuators[1];
+
+    next_state[0] = x + v * cos(psi) * dt;
+    next_state[1] = y + v * sin(psi) * dt;
+    next_state[2] = psi + v / Lf * delta * dt;
+    next_state[3] = v + a * dt;
+
+    return next_state;
 }
 
 int main() {
-  // [x, y, psi, v]
-  Eigen::VectorXd state(4);
-  // [delta, v]
-  Eigen::VectorXd actuators(2);
+    // [x, y, psi, v]
+    Eigen::VectorXd state(4);
+    // [delta, v]
+    Eigen::VectorXd actuators(2);
 
-  state << 0, 0, deg2rad(45), 1;
-  actuators << deg2rad(5), 1;
+    state << 0, 0, deg2rad(45), 1;
+    actuators << deg2rad(5), 1;
 
-  // should be [0.212132, 0.212132, 0.798488, 1.3]
-  auto next_state = globalKinematic(state, actuators, 0.3);
+    // should be [0.212132, 0.212132, 0.798488, 1.3]
+    auto next_state = globalKinematic(state, actuators, 0.3);
 
-  std::cout << next_state << std::endl;
+    std::cout << next_state << std::endl;
 }
